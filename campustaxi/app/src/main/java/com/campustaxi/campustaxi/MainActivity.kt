224705910b2f1e384740de71ee651bc1f5059dc5
minsekim1 <tkarnrwl7862@naver.com>
2021-10-30 13:42:41 +0900

    webview 1차 테스트 완료

--- campustaxi/app/src/main/java/com/campustaxi/campustaxi/MainActivity.kt
+++ campustaxi/app/src/main/java/com/campustaxi/campustaxi/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         var mWebSettings = myWebView.getSettings(); //세부 세팅 등록
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
-//        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
-//        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
-//        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
-//        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
-//        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
-//        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
-//        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-//        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
-//        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setDatabaseEnabled(true);
+
+        var dir : File = getCacheDir();
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        mWebSettings.setAppCachePath(dir.getPath());
+        mWebSettings.setAppCacheEnabled(true);
 
         myWebView.loadUrl("http://www.campus-taxi.com/")
     }
